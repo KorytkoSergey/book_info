846c31bb1f6ea121a5df7fe40b92f8318b1d3773
KorytkoSergey <korytkosergei28@gmail.com>
2023-06-04 20:32:37 +0600

    флаке зло

--- book_info/book/forms.py
+++ book_info/book/forms.py
@@ -3,10 +3,16 @@ from book import models
 
 
 class BookSearch(forms.Form):
-    title = forms.CharField(label='Поиск по названию', required=False, help_text='Поиск по названию книги')
-    year_publishing = forms.IntegerField(label='Год публикации', min_value=0, required=False,  help_text='на-р 1990')
-    publishing_house = forms.CharField(label='Издательство', required=False)
-    author = forms.CharField(label='Автор', required=False)
+    title = forms.CharField(label='Поиск по названию',
+                            required=False,
+                            help_text='Поиск по названию книги')
+    year_publishing = forms.IntegerField(label='Год публикации',
+                                         min_value=0, required=False,
+                                         help_text='на-р 1990')
+    publishing_house = forms.CharField(label='Издательство',
+                                       required=False)
+    author = forms.CharField(label='Автор',
+                             required=False)
 
     def clean(self):
         author = self.cleaned_data['author']
